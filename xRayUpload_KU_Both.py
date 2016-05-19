50ba490c5bcbb491bb43d94ff8e34503bd642a87
Jack  W King III <jack@pb-d-128-141-149-155.cern.ch>
2016-05-19 09:57:59 +0200

    corrected error in xray uploead script

--- xRayUpload_KU_Both.py
+++ xRayUpload_KU_Both.py
@@ -59,7 +59,7 @@ def analyze(inputFile, outputDir):
     unif_low = range(16)
 
     qfile = open("SummaryQPlots_XRFResult_" +inputFile+ ".txt",'r')
-    qfile = open("SummaryQplots.txt")
+    #qfile = open("SummaryQplots.txt")
     line = qfile.readlines()
     for l in range(2,len(line)):
        values = string.split(line[l])
