3e232e086d44d22fb275caaf79372a851c904a17
Jamie Antonelli <lantone@gmail.com>
2016-06-14 15:50:32 -0500

    group debris modes into one series, make individual plots for each failure mode

--- databaseTools/produceROCFailureModePlots.py
+++ databaseTools/produceROCFailureModePlots.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python                                                                                                                                                                                                                                                                        
+#!/usr/bin/env python
 import os,sys#,string,urllib2
 
-from testResultsTrackingTools import *
+from moduleTrackingTools import *
 from componentTrackingTools import *
 
 import datetime
@@ -16,87 +16,136 @@ parser.add_option("-i", "--file", dest="inputFileName",
                   help="path to input file containing a list of modules, one per line")
 (arguments, args) = parser.parse_args()
 
-from ROOT import TH1F, TCanvas, gStyle, TLegend
+from ROOT import gROOT, TH1F, TCanvas, gStyle, TLegend
 
 from ROOT import (kRed, kOrange, kYellow, kGreen,
                   kBlue, kViolet, kPink, kCyan, kBlack, kGray)
 
-partsDictionary = producePartsDictionary()
+gROOT.SetBatch()
+
+if not os.path.exists('moduleInfo.json'):
+    print "please run 'updateModuleDB.py' to produce database of module information"
+    sys.exit(0)
+
+with open('moduleInfo.json') as json_data:
+    gradeDictionary = json.load(json_data)
+
 if arguments.inputFileName:
     modules = [line.rstrip('\n') for line in open(arguments.inputFileName)]
 else:
-    modules = set(partsDictionary["module"].tolist())
-    print len(modules)
-
-if not os.path.exists('moduleGrades.json'):
-    print "please run 'updateGradeDictionary.py' to produce module grade dictionary"
-    sys.exit(0)
-
-with open('moduleGrades.json', 'r') as fp:
-    gradeDictionary = json.load(fp)
+    modules = gradeDictionary.keys()
 
 
-colors = ([kRed+1, kOrange+1, kYellow+1, kGreen+1,
+colors = (10 * [kRed+1, kOrange+1, kYellow+1, kGreen+1,
           kBlue+1, kViolet+1, kPink+1, kCyan+1, kBlack, kGray])
 
 failureModes = [
-    "Dead DC w/ good Trim",
     "Dead DC w/ bad Trim",
+    "Dead DC w/ good Trim",
+    "Bad Trim w/ no Bad DC",
     "Zombie",
     "Zero PH",
+    "Low Iana / High Vana",
+    "Iana Short",
+    "Idig Short",
     "Partially Detached",
     "Dead Pixels",
+    "Bad Bumps",
+    "No Token Pass",
+    "Unprogrammable",
+    "Pulse Height Issue",
     "Other",
 ]
 
-start_date = datetime.datetime.strptime('2015-12-01 00:00:00', '%Y-%m-%d %H:%M:%S')
+failureModeCategories = [
+    "Debris",
+    "Partially Detached",
+    "Bad Bumps",
+    "Dead Pixels",
+    "Other"
+]
+
+# group together everything we think comes from debris
+debrisFailureModes = [
+    "Dead DC w/ bad Trim",
+    "Dead DC w/ good Trim",
+    "Bad Trim w/ no Bad DC",
+    "Zombie",
+    "Zero PH",
+    "Low Iana / High Vana",
+    "Iana Short",
+    "Idig Short",
+    "No Token Pass",
+    "Unprogrammable",
+    "Pulse Height Issue",
+]
+
+
+start_date = datetime.datetime.strptime('2015-12-02 00:00:00', '%Y-%m-%d %H:%M:%S')
 
 failureModePlot = TH1F("failureModePlot", ";;# ROCs", len(failureModes), 0, len(failureModes))
 failureModePlot.SetStats(False)
 for bin in range(len(failureModes)):
     failureModePlot.GetXaxis().SetBinLabel(bin+1,str(failureModes[bin]))
 
+
 modePlots = []
-counter = 0 
+counter = 0
 for mode in failureModes:
-    plot = TH1F(mode, "ROC Failures by week;;# ROCs", 20, 0, 20)
+    plot = TH1F(mode, "ROC Failure Modes by batch;batch;ROC failure rate (%)", 30, 0, 30)
     plot.SetStats(False)
     plot.SetLineColor(colors[counter])
     plot.SetLineWidth(3)
     counter += 1
     modePlots.append(plot)
-modulesByWeekPlot = TH1F("modulesByWeek", "ROC Failures by week;;# ROCs", 20, 0, 20)
-modulesByWeekPlot.SetStats(False)
-modulesByWeekPlot.SetLineColor(1)
-modulesByWeekPlot.SetLineWidth(3)
+
+mechanismPlots = []
+counter = 0
+for mode in failureModeCategories:
+    plot = TH1F(mode+" mechanism", "ROC Failure Mechanisms by batch;batch;ROC failure rate (%)", 30, 0, 30)
+    plot.SetStats(False)
+    plot.SetLineColor(colors[counter])
+    plot.SetLineWidth(3)
+    counter += 1
+    mechanismPlots.append(plot)
+
+modulesByBatchPlot = TH1F("modulesByBatch", "ROC Failure Modes by batch;batch;ROC failure rate (%)", 30, 0, 30)
 
 nModules = 0
-nBadRocs = 0
 for module in modules:
     if module not in gradeDictionary:
         print module, "missing from grade dictionary"
         continue
-    if gradeDictionary[module]["Lessweb Grade"] == "I":
+    if gradeDictionary[module]["Grade"] == "I":
         print module, "not yet graded"
         continue
+    if pd.isnull(gradeDictionary[module]["Received"]):
+        print module, "not yet built"
+        continue
 
-#    print nModules, "-", module, gradeDictionary[module]["Lessweb Grade"]
     nModules += 1
     data = gradeDictionary[module]
-    date = partsDictionary[partsDictionary["module"] == module]["assembly date"].values[0]
+    date = str(gradeDictionary[module]["Received"])
     date_object = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
     week = math.floor((date_object - start_date).days/7.)
-    modulesByWeekPlot.Fill(week)
+    # adjust for X-mas break
+    if week >= 5:
+        week -= 2
+    modulesByBatchPlot.Fill(week)
 
-    for roc in range(15):
-        if data["ROC"+str(roc)+" Failure"]:
-            nBadRocs += 1
-#            print module, roc, data["ROC"+str(roc)+" Failure"]
-            failureModePlot.Fill(failureModes.index(data["ROC"+str(roc)+" Failure"]))
-            modePlots[failureModes.index(data["ROC"+str(roc)+" Failure"])].Fill(week)
+    for roc in range(16):
+        failureMode = data["ROC"+str(roc)+" Failure"]
+        if pd.isnull(failureMode):
+            continue
+        if failureMode in failureModes:
+            failureModePlot.Fill(failureModes.index(failureMode))
+            modePlots[failureModes.index(failureMode)].Fill(week)
+        if failureMode in failureModeCategories:
+            mechanismPlots[failureModeCategories.index(failureMode)].Fill(week)
+        elif failureMode in debrisFailureModes:
+            mechanismPlots[failureModeCategories.index("Debris")].Fill(week)
 
-canvas = TCanvas("ROCFailures")
-
+canvas = TCanvas("ROCFailures","",800,400)
 gStyle.SetTextFont(42)
 failureModePlot.SetMarkerSize(3)
 failureModePlot.Draw("bar")
@@ -112,17 +161,13 @@ failureModePlot.GetXaxis().SetLabelOffset(0.011)
 
 canvas.SaveAs("ROCFailures.pdf")
 
-
-
-
-canvas2 = TCanvas("ROCFailures")
+canvas2 = TCanvas("ROCFailuresByBatch")
 max = -999
-
 for plot in modePlots:
+    plot.Divide(modulesByBatchPlot)
+    plot.Scale(1./16*100)
     if plot.GetMaximum() > max:
         max = plot.GetMaximum()
-if modulesByWeekPlot.GetMaximum() > max:
-    max = modulesByWeekPlot.GetMaximum()
 
 legend = TLegend(0.6, 0.6, 0.9, 0.9)
 legend.SetBorderSize(0)
@@ -130,12 +175,60 @@ legend.SetFillStyle(0)
 
 
 counter = 0
-modulesByWeekPlot.Draw("c")
-legend.AddEntry(modulesByWeekPlot, "# Modules", "L")
 for plot in modePlots:
     plot.SetMaximum(1.1*max)
     legend.AddEntry(plot, failureModes[counter], "L")
     counter += 1
-    plot.Draw("same c")
+#    plot.Draw("same c")
+    plot.Draw("same")
 legend.Draw()
-canvas2.SaveAs("ROCFailureByDate.pdf")
+canvas2.SaveAs("ROCFailureModesByBatch.pdf")
+
+counter = 0
+for plot in modePlots:
+    canvas_ = TCanvas("ROCFailuresByBatch_"+failureModes[counter])
+    legend_ = TLegend(0.6, 0.6, 0.9, 0.9)
+    legend_.SetBorderSize(0)
+    legend_.SetFillStyle(0)
+    legend_.AddEntry(plot, failureModes[counter], "L")
+    plot.Draw()
+    legend_.Draw()
+    canvas_.SaveAs("ROCFailureModesByBatch_"+failureModes[counter].replace("/","ith").replace(" ","_")+".pdf")
+    counter += 1
+
+
+
+
+canvas3 = TCanvas("ROCFailureMechanismsByBatch")
+max = -999
+for plot in mechanismPlots:
+    plot.Divide(modulesByBatchPlot)
+    plot.Scale(1./16*100)
+    if plot.GetMaximum() > max:
+        max = plot.GetMaximum()
+
+legend = TLegend(0.6, 0.6, 0.9, 0.9)
+legend.SetBorderSize(0)
+legend.SetFillStyle(0)
+
+counter = 0
+for plot in mechanismPlots:
+    plot.SetMaximum(1.1*max)
+    legend.AddEntry(plot, failureModeCategories[counter], "L")
+    counter += 1
+#    plot.Draw("same c")
+    plot.Draw("same")
+legend.Draw()
+canvas3.SaveAs("ROCFailureMechanismsByBatch.pdf")
+
+counter = 0
+for plot in mechanismPlots:
+    canvas_ = TCanvas("ROCFailuresByBatch_"+failureModeCategories[counter])
+    legend_ = TLegend(0.6, 0.6, 0.9, 0.9)
+    legend_.SetBorderSize(0)
+    legend_.SetFillStyle(0)
+    legend_.AddEntry(plot, failureModeCategories[counter], "L")
+    plot.Draw()
+    legend_.Draw()
+    canvas_.SaveAs("ROCFailureMechanismsByBatch_"+failureModeCategories[counter].replace("/","ith").replace(" ","_")+".pdf")
+    counter += 1
