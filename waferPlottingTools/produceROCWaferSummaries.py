9602ebfbeda859a2c106b817b03182ddeb1b6399
Jamie Antonelli <lantone@gmail.com>
2016-09-07 09:19:39 -0500

    updated to keep up with changes in other tools

--- waferPlottingTools/produceROCWaferSummaries.py
+++ waferPlottingTools/produceROCWaferSummaries.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python                                                                                                                                                                                     
+#!/usr/bin/env python
 from databaseTools.componentTrackingTools import *
-from moduleSummaryPlottingTools import *
+from waferPlottingTools import *
 import sys
 import glob
 
 gROOT.SetBatch()
 
-moduleReportFile = '/Users/lantonel/FPIXUtils/modulereport.csv'
-resultsDirectory = '/Users/lantonel/PlotsAndTables/ModuleTestResults/'
+resultsDirectory = '/Users/lantonel/PlotsAndTables/UniqueTestResults/'
 
 # parse list of module components
 partsDictionary = producePartsDictionary()
@@ -22,11 +21,16 @@ for wafer in listOfWafers:
     plotDictionary[wafer] = []
 
 counter = 1
-for name in glob.glob(resultsDirectory + 'M*.root'):
-    moduleName = name.split("/")[5].split("_")[0]
+listOfDirs = glob.glob(resultsDirectory + '/M*/*/commander*.root')
+for name in listOfDirs:
+    moduleName = name.split("/")[-3].split("_")[0]
+
+#    if moduleName != 'M-Y-T-NP':
+#        continue
 
     print "module #" + str(counter) + ": " +  moduleName
     counter += 1
+
 
 # choose histogram to use for summary plot
 #    inputPath = 'Scurves/sig_scurveVcal_Vcal'
@@ -35,17 +39,18 @@ for name in glob.glob(resultsDirectory + 'M*.root'):
 
     plots = produce2DPlotList(name, inputPath)
     if plots is None:
+        print "no valid plots"
         continue
 
 # convert input to binary plots if desired
 #    makeBinaryPlots(plots,-0.1,0.1)
-#    makeBinaryPlots(plots,5,-5)
-
+#    makeBinaryPlots(plots,5,-50)
 
     for chipIndex in range(16):
         rocCoordinates = getROCCoordinates(partsDictionary, moduleName, chipIndex)
         wafer = rocCoordinates[0]
         if wafer == "?":
+            print "no wafer found, skipping"
             continue
         pad = int(rocCoordinates[1]) * 10 + int(rocCoordinates[2]) + 1
         subpad = ord(rocCoordinates[3]) - ord('A') + 1
@@ -59,3 +64,4 @@ for name in glob.glob(resultsDirectory + 'M*.root'):
 for wafer in listOfWafers:
     if len(plotDictionary[wafer]):
         saveROCWaferCanvas(wafer, inputPath, plotDictionary, -5, 5)
+#        saveROCWaferCanvas(wafer, inputPath, plotDictionary, 0, 1)
